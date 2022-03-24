1a79f6c4acd1ee1f2f851b9b7a11ef8fc762a52f
josslynm <88407635+josslynm@users.noreply.github.com>
2022-03-23 22:00:11 -0600

    Se realizo actividad1 con index1 para visualizar

---
+++ actividad1.js
@@ -0,0 +1,22 @@
+//  MULTIPLICACION
+
+var factor = Number(prompt("Ingresa el primer factor para multiplicar"));
+var multiplicacion = factor*2;
+alert("El resultado de multiplicar x2 es " + multiplicacion);
+
+var sumando1= Number( prompt("Ingresa el primer número"));
+var sumando2= Number( prompt("Ingresa el segundo número"));
+
+var resta = sumando1 - sumando2;
+alert("El resultado de la diferencia es " + resta );
+
+
+var radio = Number(prompt("Ingresa el radio del círculo"))
+var areac = (Math.PI  * (radio ** 2));
+alert("El área es " + areac + " [u^2]");
+
+var D = Number(prompt("Ingresa diagonal mayor"))
+var d = Number(prompt("Ingresa diagonal menor"))
+var arear = (D*d)/2;
+alert("El área es " + arear + " [u^2]");
+
