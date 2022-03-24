19749ee7332305ea8dc03a44f78a8914a58a8b52
josslynm <88407635+josslynm@users.noreply.github.com>
2022-03-24 15:34:39 -0600

    Se modfico y arregló tamaño de arrays

--- actividad-05.js
+++ actividad-05.js
@@ -4,20 +4,20 @@ JOCELYN MEJIA */
 //1.
  var arreglo=[1,4,6,10,22,55,46,2,5,0];
 
-for(var i=0; i<= arreglo.length ; i++)
-    console.log(arreglo[i]);
+for(var i=0; i< arreglo.length ; i++)
+    console.log("arreglo["+i+"]"+arreglo[i]);
 
-for(var i=0; i<= arreglo.length ; i++){
+for(var i=0; i< arreglo.length ; i++){
 
     if(arreglo[i] > 3)
-        console.log(arreglo[i]);
+        console.log("arreglo["+i+"]"+arreglo[i]);
 }
    
 //2.
 
 var arreglo2 = [];
 var k=0;
-while(k <= 4){
+while(k < 4){
 
     arreglo2[k] =  Number(prompt("valor [" +k+ "]"))
 
@@ -25,5 +25,5 @@ while(k <= 4){
 
 }
 
-for(var i=0; i <= arreglo2.length-1 ; i++)
+for(var i=0; i < arreglo2.length ; i++)
     console.log("arreglo2["+ i +"]= " +arreglo2[i]);
