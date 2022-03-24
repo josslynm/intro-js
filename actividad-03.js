9f267ac80a0634c686ece5f7acdf7a5abaef1800
josslynm <88407635+josslynm@users.noreply.github.com>
2022-03-24 11:03:01 -0600

    Resuelta actividad03

--- actividad-03.js
+++ actividad-03.js
@@ -27,13 +27,33 @@ if(variable3 === 1000){
 console.log("Lo sentimos, sigue participando");
 
 //4
-
 var numero1 = Number(prompt("Ingresa un número:"));
 var numero2 = Number(prompt("Ingresa un número:"));
 
 if(numero1 != numero2 && numero1>numero2 )
 {
     console.log(numero2 + " es menor")
-}else
+}else if( numero1 != numero2 && numero2>numero1)
     console.log(numero1 + " es menor")
+
+//5
+var numA = Number(prompt("Ingresa un número:"));
+var numB = Number(prompt("Ingresa un número:"));
+var numC = Number(prompt("Ingresa un número:"));
+
+var aux=0;
+
+if(numA > numB || numA == numB){
+    aux = numA;
+    console.log("El numero mayor es ", numA);
+}else if (numB > numA){
+    aux =numB;
+    console.log("El numero mayor es ", numA);
+}
+
+if(aux > numC){
+    console.log(aux + " es el numero mayor");
+}else if( numC > aux || aux == numC)
+    console.log("El numero mayor es " + numC);
+
 
