036eb79597cfcee93f3b0ae66b39671aa936ce07
josslynm <88407635+josslynm@users.noreply.github.com>
2022-03-28 17:14:07 -0600

    Actividad 09 y 10

---
+++ actividad-10.js
@@ -0,0 +1,45 @@
+/**
+ * Find the difference in age between the oldest and youngest family members, 
+ * and return their respective ages and the age difference.
+ * 
+ */
+ ​
+ const Family = [
+    {
+      name: 'John',
+      age: 13
+    },
+    {
+      name: 'Mark',
+      age: 56,
+    },
+    {
+      name: 'Rachel',
+      age: 45,
+    },
+    {
+      name: 'Nate',
+      age: 67,
+    },
+    {
+      name: 'Jeniffer',
+      age: 65,
+    }
+  ];
+​
+​
+/**
+ * If the given input is an array of numbers, return the sum of all the positives ones. 
+ * If the array is empty or there aren't any positive numbers, return 0.
+ */
+​
+ const numbers = [ 1, -4, 12, 0, -3, 29, -150];
+​
+​
+ /**
+ * Escriba una función de JavaScript que tomará una serie de números almacenados
+ * y encontrará el segundonúmeros más bajos y segundos más altos, respectivamente
+ * del siguiente arreglo:  const arr = [3,4,6,1,5,2,9,10,23,12]
+ */
+​
+  const arr = [3,4,6,1,5,2,9,10,23,12]
